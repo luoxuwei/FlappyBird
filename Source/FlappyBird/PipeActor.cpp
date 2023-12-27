9b98faf6c765e3152e20b1857c7b5715c47c99cc
luoxuwei <xwluo@foxmail.com>
2023-12-28 03:29:55 +0800

    创建管道

--- Source/FlappyBird/PipeActor.cpp
+++ Source/FlappyBird/PipeActor.cpp
@@ -3,6 +3,8 @@
 
 #include "PipeActor.h"
 #include <Components/SceneComponent.h>
+#include <PaperSprite.h>
+#include <PaperSpriteComponent.h>
 
 // Sets default values
 APipeActor::APipeActor()
@@ -11,11 +13,20 @@ APipeActor::APipeActor()
 	PrimaryActorTick.bCanEverTick = true;
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
 
+	ConstructorHelpers::FObjectFinder<UPaperSprite> UpSpriteObj(TEXT("PaperSprite'/Game/FlappyBird/Textures/Pipes/pipe_up_Sprite.pipe_up_Sprite'"));
+	ConstructorHelpers::FObjectFinder<UPaperSprite> DownSpriteObj(TEXT("PaperSprite'/Game/FlappyBird/Textures/Pipes/pipe_down_Sprite.pipe_down_Sprite'"));
+
 	for (size_t i = 0; i < 3; i++)
 	{
 		USceneComponent *c = CreateDefaultSubobject<USceneComponent>(*FString::Printf(TEXT("GroupComp%d"), i));
 		c->SetupAttachment(RootComponent);
 		PipeGroup.Add(c);
+		UPaperSpriteComponent* SpriteUp = CreateDefaultSubobject<UPaperSpriteComponent>(*FString::Printf(TEXT("Group%dPipeUp"), i));
+		UPaperSpriteComponent* SpriteDown = CreateDefaultSubobject<UPaperSpriteComponent>(*FString::Printf(TEXT("Group%dPipeDown"), i));
+		SpriteUp->SetupAttachment(c);
+		SpriteUp->SetSprite(UpSpriteObj.Object);
+		SpriteDown->SetupAttachment(c);
+		SpriteDown->SetSprite(DownSpriteObj.Object);
 	}
 
 }
